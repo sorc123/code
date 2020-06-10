2a38a299c52deea5ae73be72523296f7d978c9a8
sorc123 <41237013+sorc123@users.noreply.github.com>
2020-06-10 19:12:01 +0200

    Add files via upload

---
+++ Numerolosko_slaganje/Scripts/index.js
@@ -0,0 +1,32 @@
+﻿var dugme = document.getElementById("izracunaj");
+
+function slaganje(datum) {
+    var sum = 0;
+    for (var i = 0; i < datum.length; ++i) {
+        if (datum[i] != '.')
+            sum += Number(datum[i]);
+    }
+    if (sum > 9) {
+        sum = slaganje(sum.toString());
+    }   
+    return sum;
+}
+
+dugme.addEventListener("click", function ()
+{
+    document.getElementById("prva").innerHTML = document.getElementById("ime1").value;
+    document.getElementById("druga").innerHTML = document.getElementById("ime2").value;
+    var datum1 = document.getElementById("datum1").value;
+    var datum2 = document.getElementById("datum2").value;
+    var sum1 = 0;
+    var sum2 = 0;
+
+    if (datum1 != "" && datum2 != "")
+    {
+        sum1 = slaganje(datum1);
+        sum2 = slaganje(datum2);
+        document.getElementById("rezultat").innerHTML = "" + (sum1 / sum2 * 100) + "%";
+    }
+});
+
+
---
+++ Rezervacija_autobuskih_karata/JS/index.js
@@ -0,0 +1,66 @@
+﻿var autobus = document.getElementById("autobus");
+var dugme = document.getElementById("dugme");
+var x = 15, y = 70;
+var izabrano;
+
+function novo_sediste(br_sedista)
+{
+    var sediste = document.createElement("div");
+    var p = document.createElement("p");
+    var tekst = document.createTextNode(br_sedista);
+    p.appendChild(tekst);
+    
+    sediste.setAttribute("class", "sediste");
+    
+    sediste.style.top = y + "px";
+    sediste.style.left = x + "px";
+
+    sediste.appendChild(p);
+    autobus.appendChild(sediste);
+
+    sediste.addEventListener("click", function ()
+    {
+        var textbox = document.getElementById("brojsedista");
+        textbox.value = this.childNodes[0].innerHTML;
+        izabrano = this.childNodes[0].innerHTML;
+    });
+
+    if (br_sedista % 2 == 0)
+    {
+        x += 35;
+    }
+    else if ((br_sedista - 1) % 4 == 0) {
+        x = 15;
+        y += 35;
+    }
+    else
+    {
+        x += 70;
+    }
+    
+    br_sedista++;
+}
+
+for (var i = 2; i <= 52; ++i)
+{
+    novo_sediste(i);
+}
+
+document.addEventListener("submit", function () {
+        var sed = document.getElementsByClassName("sediste");
+
+        for (var i = 0; i < sed.length; ++i) {
+            if (sed[i].childNodes[0].innerHTML == izabrano) {
+                sed[i].style.backgroundColor = "red";
+                break;
+            }
+        }
+        event.preventDefault();
+
+
+});
+
+
+
+
+
